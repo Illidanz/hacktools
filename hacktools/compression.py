c81b1fcdd03c115eb683d6f71ce8c2c20374ef8b
Illidanz <illidanrulez@gmail.com>
2019-09-09 19:26:49 +0200

    Added LZ11 compression

--- hacktools/compression.py
+++ hacktools/compression.py
@@ -75,35 +75,6 @@ def decompressLZ10(data, complength, decomplength, dispextra=1):
         return out.read(decomplength)
 
 
-def getOccurrenceLength(indata, newptr, newlength, oldptr, oldlength, mindisp=1):
-    disp = 0
-    if newlength == 0:
-        return 0
-    maxlength = 0
-    # try every possible 'disp' value (disp = oldLength - i)
-    for i in range(oldlength - mindisp):
-        # work from the start of the old data to the end, to mimic the original implementation's behaviour
-        # (and going from start to end or from end to start does not influence the compression ratio anyway)
-        currentoldstart = oldptr + i
-        currentlength = 0
-        # determine the length we can copy if we go back (oldLength - i) bytes
-        # always check the next 'newLength' bytes, and not just the available 'old' bytes,
-        # as the copied data can also originate from what we're currently trying to compress.
-        for j in range(newlength):
-            # stop when the bytes are no longer the same
-            if indata[currentoldstart + j] != indata[newptr + j]:
-                break
-            currentlength += 1
-        # update the optimal value
-        if currentlength > maxlength:
-            maxlength = currentlength
-            disp = oldlength - i
-            # if we cannot do better anyway, stop trying.
-            if maxlength == newlength:
-                break
-    return maxlength, disp
-
-
 def compressLZ10(indata, mindisp=1):
     with common.Stream() as out:
         inlength = len(indata)
@@ -189,6 +160,99 @@ def decompressLZ11(data, complength, decomplength):
             header <<= 1
 
 
-def compressLZ11(indata):
-    # TODO
-    return bytes()
+def compressLZ11(indata, mindisp=1):
+    with common.Stream() as out:
+        inlength = len(indata)
+        compressedlength = 0
+        instart = 0
+        # we do need to buffer the output, as the first byte indicates which blocks are compressed.
+        # this version does not use a look-ahead, so we do not need to buffer more than 8 blocks at a time.
+        # (a block is at most 4 bytes long)
+        outbuffer = bytearray(8 * 4 + 1)
+        outbuffer[0] = 0
+        bufferlength = 1
+        bufferedblocks = 0
+        readbytes = 0
+        while readbytes < inlength:
+            # If 8 blocks are bufferd, write them and reset the buffer
+            # we can only buffer 8 blocks at a time.
+            if bufferedblocks == 8:
+                out.write(outbuffer[:bufferlength])
+                compressedlength += bufferlength
+                # reset the buffer
+                outbuffer[0] = 0
+                bufferlength = 1
+                bufferedblocks = 0
+            # determine if we're dealing with a compressed or raw block.
+            # it is a compressed block when the next 3 or more bytes can be copied from
+            # somewhere in the set of already compressed bytes.
+            oldlength = min(readbytes, 0x1000)
+            length, disp = getOccurrenceLength(indata, instart + readbytes, min(inlength - readbytes, 0x10110), instart + readbytes - oldlength, oldlength, mindisp)
+            # length not 3 or more? next byte is raw data
+            if length < 3:
+                outbuffer[bufferlength] = indata[instart + readbytes]
+                bufferlength += 1
+                readbytes += 1
+            else:
+                # 3 or more bytes can be copied? next (length) bytes will be compressed into 2 bytes
+                readbytes += length
+                # mark the next block as compressed
+                outbuffer[0] |= (1 << (7 - bufferedblocks))
+                if length >= 0x110:
+                    # case 1: 1(B CD E)(F GH) + (0x111)(0x1) = (LEN)(DISP)
+                    outbuffer[bufferlength] = 0x10
+                    outbuffer[bufferlength] |= ((length - 0x111) >> 12) & 0x0F
+                    bufferlength += 1
+                    outbuffer[bufferlength] = ((length - 0x111) >> 4) & 0xFF
+                    bufferlength += 1
+                    outbuffer[bufferlength] = ((length - 0x111) << 4) & 0xF0
+                elif length > 0x10:
+                    # case 0; 0(B C)(D EF) + (0x11)(0x1) = (LEN)(DISP)
+                    outbuffer[bufferlength] = 0x00
+                    outbuffer[bufferlength] |= ((length - 0x11) >> 4) & 0x0F
+                    bufferlength += 1
+                    outbuffer[bufferlength] = ((length - 0x11) << 4) & 0xF0
+                else:
+                    # case > 1: (A)(B CD) + (0x1)(0x1) = (LEN)(DISP)
+                    outbuffer[bufferlength] = ((length - 1) << 4) & 0xF0
+                # the last 1.5 bytes are always the disp
+                outbuffer[bufferlength] |= ((disp - 1) >> 8) & 0x0F
+                bufferlength += 1
+                outbuffer[bufferlength] = (disp - 1) & 0xFF
+                bufferlength += 1
+            bufferedblocks += 1
+        # copy the remaining blocks to the output
+        if bufferedblocks > 0:
+            out.write(outbuffer[:bufferlength])
+            compressedlength += bufferlength
+        out.seek(0)
+        return out.read()
+
+
+def getOccurrenceLength(indata, newptr, newlength, oldptr, oldlength, mindisp=1):
+    disp = 0
+    if newlength == 0:
+        return 0
+    maxlength = 0
+    # try every possible 'disp' value (disp = oldLength - i)
+    for i in range(oldlength - mindisp):
+        # work from the start of the old data to the end, to mimic the original implementation's behaviour
+        # (and going from start to end or from end to start does not influence the compression ratio anyway)
+        currentoldstart = oldptr + i
+        currentlength = 0
+        # determine the length we can copy if we go back (oldLength - i) bytes
+        # always check the next 'newLength' bytes, and not just the available 'old' bytes,
+        # as the copied data can also originate from what we're currently trying to compress.
+        for j in range(newlength):
+            # stop when the bytes are no longer the same
+            if indata[currentoldstart + j] != indata[newptr + j]:
+                break
+            currentlength += 1
+        # update the optimal value
+        if currentlength > maxlength:
+            maxlength = currentlength
+            disp = oldlength - i
+            # if we cannot do better anyway, stop trying.
+            if maxlength == newlength:
+                break
+    return maxlength, disp
