3eec8f465e9eda7dd363797ca2c9791f44d48f91
Illidanz <illidanrulez@gmail.com>
2019-10-01 13:30:16 +0200

    Added brfnt support

--- hacktools/wii.py
+++ hacktools/wii.py
@@ -1,5 +1,5 @@
 import os
-from hacktools import common
+from hacktools import common, nitro
 
 
 def extractIso(isofile, extractfolder, workfolder=""):
@@ -17,3 +17,52 @@ def repackIso(isofile, isopatch, workfolder, patchfile=""):
         os.remove(isopatch)
     common.execute("wit COPY {folder} {iso}".format(folder=workfolder, iso=isopatch), False)
     common.logMessage("Done!")
+
+
+def getFontGlyphs(file):
+    glyphs = {}
+    with common.Stream(file, "rb", False) as f:
+        # Header
+        f.seek(36)
+        hdwcoffset = f.readUInt()
+        pamcoffset = f.readUInt()
+        common.logDebug("hdwcoffset:", hdwcoffset, "pamcoffset:", pamcoffset)
+        # HDWC
+        f.seek(hdwcoffset - 4)
+        hdwclen = f.readUInt()
+        tilenum = (hdwclen - 16) // 3
+        firstcode = f.readUShort()
+        lastcode = f.readUShort()
+        f.seek(4, 1)
+        common.logDebug("firstcode:", firstcode, "lastcode:", lastcode, "tilenum", tilenum)
+        hdwc = []
+        for i in range(tilenum):
+            hdwcstart = f.readSByte()
+            hdwcwidth = f.readByte()
+            hdwclength = f.readByte()
+            hdwc.append((hdwcstart, hdwcwidth, hdwclength))
+        # PAMC
+        nextoffset = pamcoffset
+        while nextoffset != 0x00:
+            f.seek(nextoffset)
+            firstchar = f.readUShort()
+            lastchar = f.readUShort()
+            sectiontype = f.readUShort()
+            f.seek(2, 1)
+            nextoffset = f.readUInt()
+            common.logDebug("firstchar:", common.toHex(firstchar), "lastchar:", common.toHex(lastchar), "sectiontype:", sectiontype, "nextoffset:", nextoffset)
+            if sectiontype == 0:
+                firstcode = f.readUShort()
+                for i in range(lastchar - firstchar + 1):
+                    c = nitro.codeToChar(firstchar + i)
+                    glyphs[c] = hdwc[firstcode + i] + (firstchar + i,)
+            elif sectiontype == 1:
+                for i in range(lastchar - firstchar + 1):
+                    charcode = f.readUShort()
+                    if charcode == 0xFFFF or charcode >= len(hdwc):
+                        continue
+                    c = nitro.codeToChar(firstchar + i)
+                    glyphs[c] = hdwc[charcode] + (firstchar + i,)
+            else:
+                common.logError("Unknown section type", sectiontype)
+    return glyphs
