423319549695ff6a66c6618362c86859ec2b151c
Illidan <illidanrulez@gmail.com>
2023-03-14 10:16:17 +0100

    Moved version to __init__.py

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
+import codecs
+import os.path
 from setuptools import setup, Extension
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+def getversion(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 extras_nds=["ndspy"]
 extras_armips=["pyarmips"]
@@ -14,7 +29,7 @@ extras_cli=["click", "tqdm", "customtkinter"]
 
 setup(
     name="hacktools",
-    version="0.32.0",
+    version=getversion("hacktools/__init__.py"),
     author="Illidan",
     description="A set of utilities and tools for rom hacking and translations.",
     long_description=long_description,
