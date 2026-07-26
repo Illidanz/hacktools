3a77cffc35d2d407abf96b45d935a7abb091332a
Illidan <illidanrulez@gmail.com>
2026-07-26 08:29:02 +0200

    Added RLE compression.

--- hacktools/c_ext/cmp_misc.c
+++ hacktools/c_ext/cmp_misc.c
@@ -54,8 +54,98 @@ static PyObject* decompressRLE(PyObject* m, PyObject* args, PyObject* kwargs)
     return output;
 }
 
+// Write a pending raw block, if any, and return the updated output length
+static unsigned int flushRawBlock(unsigned char* out, unsigned int compressedlength, unsigned char* rawblock, unsigned int rawlength)
+{
+    if (rawlength > 0)
+    {
+        out[compressedlength++] = (unsigned char)(rawlength - 1);
+        for (unsigned int i = 0; i < rawlength; ++i)
+            out[compressedlength++] = rawblock[i];
+    }
+    return compressedlength;
+}
+
+PyDoc_STRVAR(compressRLE_doc,
+"compressRLE($module, /, indata)\n"
+"--\n"
+"\n"
+"Compress data with the RLE scheme, as used by the GBA and NDS BIOS.\n"
+"\n"
+"Args:\n"
+"    indata (bytes): Data to compress.\n"
+"\n"
+"Returns:\n"
+"    bytes: The compressed data, without the 4-byte header.");
+
+static PyObject* compressRLE(PyObject* m, PyObject* args, PyObject* kwargs)
+{
+    static char *kwlist[] = { "indata", NULL };
+
+    unsigned char* indata;
+    size_t inlength;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s#", kwlist, &indata, &inlength))
+        return NULL;
+
+    // Worst case: uncompressible data emits 1 flag byte per 128 raw bytes,
+    // so output can reach inlength + ceil(inlength/128)
+    size_t outcap = inlength + (inlength + 127) / 128 + 32;
+    unsigned char* out = PyMem_Malloc(outcap);
+    MALLOC_CHECK(out);
+
+    unsigned int compressedlength = 0;
+    // bytes that could not be compressed are buffered until the block is full
+    // or until a run is found, as their amount is written before them
+    unsigned char rawblock[0x80];
+    unsigned int rawlength = 0;
+    size_t readbytes = 0;
+    while (readbytes < inlength)
+    {
+        // determine how many times the current byte is repeated,
+        // a run is at most 0x7f + 3 bytes long
+        unsigned char byte = indata[readbytes++];
+        unsigned int repcount = 1;
+        while (readbytes < inlength && repcount < 0x82 && indata[readbytes] == byte)
+        {
+            ++repcount;
+            ++readbytes;
+        }
+        if (repcount >= 3)
+        {
+            // 3 or more repetitions are worth a compressed block,
+            // which has to come after the raw bytes read until now
+            compressedlength = flushRawBlock(out, compressedlength, rawblock, rawlength);
+            rawlength = 0;
+            out[compressedlength++] = (unsigned char)(0x80 | (repcount - 3));
+            out[compressedlength++] = byte;
+        }
+        else
+        {
+            // less than 3, buffer them as raw bytes,
+            // a raw block is at most 0x7f + 1 bytes long
+            for (unsigned int i = 0; i < repcount; ++i)
+            {
+                rawblock[rawlength++] = byte;
+                if (rawlength == 0x80)
+                {
+                    compressedlength = flushRawBlock(out, compressedlength, rawblock, rawlength);
+                    rawlength = 0;
+                }
+            }
+        }
+    }
+    // write the remaining raw bytes
+    compressedlength = flushRawBlock(out, compressedlength, rawblock, rawlength);
+
+    PyObject *output = PyBytes_FromStringAndSize(out, compressedlength);
+    PyMem_Free(out);
+    return output;
+}
+
 static PyMethodDef Cmp_miscMethods[] = {
     {"decompressRLE", (PyCFunction)decompressRLE, METH_VARARGS | METH_KEYWORDS, decompressRLE_doc},
+    {"compressRLE", (PyCFunction)compressRLE, METH_VARARGS | METH_KEYWORDS, compressRLE_doc},
     {NULL, NULL, 0, NULL}
 };
 
